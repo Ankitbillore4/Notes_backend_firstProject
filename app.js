ab104b6e536522c84bd54bbeaf9b73190de03745
billoreankit66@gmail.com <billoreankit66@gmail.com>
2024-10-16 00:01:44 +0530

    app.js changes

--- app.js
+++ app.js
@@ -4,35 +4,11 @@ const app= express();
 app.set("view engine","ejs");
 
 app.get("/",function(req,res){
-    fs.readdir("uploads", function(err,files){
-        if (err){
-          console.log(err);
-        }
-        const notes= []
-        for (const file of files){
-            const description = fs.readFileSync(`uploads/${file}`,"utf-8")
-            notes.push({
-                title:file,
-                description:description
-            })
-        }
-        res.render("index",{notes})  
-      })
-    
+    res.render("index")
 })
 
-app.get("/create", (req, res) => {
-    res.render("create");
-})
-app.get("/create", (req, res) => {
-    res.render("create");
-})
- 
 app.get("/notes",function(req,res){
     res.render("notes")
-}) 
+})
 
-app.listen(3000,()=>{
-    console.log("server is running :");
-    
-});
+app.listen(3000);
