8deace9bd0cd956a5c415815643828488e2221fa
billoreankit66@gmail.com <billoreankit66@gmail.com>
2024-10-15 23:35:09 +0530

    js changes

--- app.js
+++ app.js
@@ -4,7 +4,21 @@ const app= express();
 app.set("view engine","ejs");
 
 app.get("/",function(req,res){
-    res.render("index")
+    fs.readdir("uploads", function(err,files){
+        if (err){
+          console.log(err);
+        }
+        const notes= []
+        for (const file of files){
+            const description = fs.readFileSync(`uploads/${file}`,"utf-8")
+            notes.push({
+                title:file,
+                description:description
+            })
+        }
+        res.render("index",{notes})  
+      })
+    
 })
  
 app.get("/notes",function(req,res){
